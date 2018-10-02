f8f3c357e9011def598b8add35756827d19ca8af
idstck <idstackdeveloper@gmail.com>
2018-10-02 22:31:35 +0700

    Upload Asset and Templating pt1

---
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="left side-menu">
+    <div class="sidebar-inner slimscrollleft">
+        <!--- Divider -->
+        <div id="sidebar-menu">
+            <ul>
+
+                <li class="text-muted menu-title">Navigation</li>
+
+                <li class="">
+                    <a href="{{ route('home') }}" class="waves-effect"><i class="ti-home"></i> <span> Dashboard </span></a>
+                </li>
+
+                <li class="">
+                    <a href="#1" class="waves-effect"><i class="ti-user"></i> <span> Users </span></a>
+                </li>
+
+                <li class="text-muted menu-title">More</li>
+
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="ti-share"></i><span>Multi Level </span> <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li class="has_sub">
+                            <a href="javascript:void(0);" class="waves-effect"><span>Menu Level 1.1</span>  <span class="menu-arrow"></span></a>
+                            <ul style="">
+                                <li><a href="javascript:void(0);"><span>Menu Level 2.1</span></a></li>
+                                <li><a href="javascript:void(0);"><span>Menu Level 2.2</span></a></li>
+                                <li><a href="javascript:void(0);"><span>Menu Level 2.3</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);"><span>Menu Level 1.2</span></a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
